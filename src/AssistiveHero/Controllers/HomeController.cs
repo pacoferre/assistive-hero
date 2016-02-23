5c39e94cdfc99a1f2baae8de0fa764b8cee8ba63
Paco Ferre <pacoferre@gmail.com>
2016-02-23 21:11:31 +0100

    First version

---
+++ src/AssistiveHero/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using System.Speech.Synthesis;
+using System.IO;
+using AssistiveHero.Models;
+
+namespace AssistiveHero.Controllers
+{
+    [ResponseCache(Duration = 60)]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public JsonResult GetDemo()
+        {
+            return new JsonResult(Models.Item.GetDemo());
+        }
+
+        private static object lOb = new object();
+
+        [HttpGet]
+        public FileResult Play(string text)
+        {
+            List<Item> items = Models.Item.GetDemo();
+
+            if (!Item.CanSay(ref items, text))
+            {
+                text = "Error";
+            }
+
+            lock(lOb)
+            {
+                using (var synthesizer = new SpeechSynthesizer())
+                {
+                    var ms = new MemoryStream();
+
+                    foreach (var v in synthesizer.GetInstalledVoices())
+                    {
+                        if (v.VoiceInfo.Culture.Name.Contains("es"))
+                        {
+                            synthesizer.SelectVoice(v.VoiceInfo.Name);
+                            break;
+                        }
+                    }
+
+                    synthesizer.SetOutputToWaveStream(ms);
+                    synthesizer.Speak(text);
+
+                    ms.Position = 0;
+                    return new FileStreamResult(ms, "audio/wav");
+                }
+            }
+        }
+    }
+}
---
+++ src/AssistiveHero/Models/Item.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssistiveHero.Models
+{
+    public class Item
+    {
+        public string url = "";
+        public string text = "";
+        public bool disabled = false;
+        public List<Item> childreen = null;
+
+        public Item(string url, string text, bool disabled)
+        {
+            this.url = url;
+            this.text = text;
+            this.disabled = disabled;
+        }
+
+        public static List<Item> demo = new List<Item>();
+
+        public static List<Item> GetDemo()
+        {
+            if (demo.Count == 0)
+            {
+                lock (demo)
+                {
+                    if (demo.Count == 0)
+                    {
+                        Item quiero = new Item("/images/demo/quiero.jpg", "Quiero", false);
+                        Item colegio = new Item("/images/demo/colegio.jpg", "Colegio", false);
+                        Item frutas = new Item("/images/demo/frutas.jpg", "Frutas", false);
+
+                        demo.Add(quiero);
+
+                        demo.Add(colegio);
+
+                        quiero.childreen = new List<Item>(5);
+                        colegio.childreen = new List<Item>(5);
+
+
+                        colegio.childreen.Add(new Item("/images/demo/recreo.jpg", "Recreo", false));
+                        colegio.childreen.Add(new Item("/images/demo/pintar.jpg", "Pintar", false));
+
+                        quiero.childreen.Add(new Item("/images/demo/agua.jpg", "Agua", false));
+                        quiero.childreen.Add(new Item("/images/demo/pan.jpg", "Pan", false));
+
+                        quiero.childreen.Add(frutas);
+
+                        frutas.childreen = new List<Item>(5);
+                        frutas.childreen.Add(new Item("/images/demo/manzana.jpg", "Manzana", false));
+                        frutas.childreen.Add(new Item("/images/demo/naranja.jpg", "Naranja", false));
+                        frutas.childreen.Add(new Item("/images/demo/pera.jpg", "Pera", false));
+                        frutas.childreen.Add(new Item("/images/demo/uvas.jpg", "Uvas", true));
+                        frutas.childreen.Add(new Item("/images/demo/platano.jpg", "Plátano", false));
+                    }
+                }
+            }
+
+            return demo;
+        }
+
+        public static bool CanSay(ref List<Item> items, string text)
+        {
+            foreach(Item item in items)
+            {
+                if (item.text == text)
+                {
+                    return true;
+                }
+                if (item.childreen != null)
+                {
+                    if (CanSay(ref item.childreen, text))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
